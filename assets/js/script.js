d2dd89d2eca0ab43dc51635e9ca52dddeb4e29e5
Deividhsn <deividhsn@gmail.com>
2024-07-25 20:54:38 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function openBox(){
+    var element = document.getElementById("oi");
+        element.classList.add("io");
+}
+function closeBox(){
+    var element = document.getElementById("oi");
+        element.classList.remove("io");
+}
